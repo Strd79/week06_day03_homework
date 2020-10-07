2e638d73ed6183d5cd654dc42bb3baff480629ba
David Strain <strd79@hotmail.co.uk>
2020-10-07 18:19:08 +0100

    Second commit files hadn't been added

--- hw_enumeration_traveller_start/models/traveller.js
+++ hw_enumeration_traveller_start/models/traveller.js
@@ -3,27 +3,56 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  return this.journeys.map((journey) => {
+    return journey.startLocation;
+  });
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter((journey) => {
+    if (journey.transport === transport) {
+      return journey;
+    };
+  });
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter((journey) => {
+    if (journey.distance > minDistance) {
+      return journey;
+    };
+  });
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  const total = this.journeys.reduce((runningTotal, journey) => {
+    return runningTotal + journey.distance;
+  }, 0);
+  return total;
 };
 
-Traveller.prototype.getUniqueModesOfTransport = function () {
+// Traveller.prototype.getUniqueModesOfTransport = function () {
+//  const modesOfTransport = this.journeys.map((journey) => {
+//   return journey.transport;
+//  });
+//  const uniqueModesOfTransport = modesOfTransport.filter((transport, index) => {
+//     modesOfTransport.indexOf(transport) === index;
+// });
+//   return uniqueModesOfTransport;
+// };
 
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  const modesOfTransport = this.journeys.map((journey) => {
+    return journey.transport;
+  });
+  const uniqueModesOfTransport = [...new Set(modesOfTransport)];
+  return uniqueModesOfTransport;
 };
 
 
